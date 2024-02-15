89b49d73a754f6df3ccb31918d926759c7e8a1f5
levimaciell <levimaciell1221@gmail.com>
2024-02-15 19:44:56 -0300

    Adding subtraction, multiply, division, average and square root operations

--- src/main/java/com/levi/spring/math/MathController.java
+++ src/main/java/com/levi/spring/math/MathController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.levi.spring.math.exceptions.UnsupportedMathOperationException;
+
 @RestController
 public class MathController {
     
@@ -14,9 +16,63 @@ public class MathController {
         @PathVariable(value = "num2") String num2) throws Exception{
 
         if(!isNumeric(num1) || !isNumeric(num2)){
-            throw new Exception();
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
         return convertToDouble(num1) + convertToDouble(num2);
+    }
+
+    @RequestMapping(value = "/subtraction/{num1}/{num2}", method = RequestMethod.GET)
+    public Double subtraction(
+        @PathVariable(value = "num1") String num1,
+        @PathVariable(value = "num2") String num2) throws Exception{
+
+        if(!isNumeric(num1) || !isNumeric(num2)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(num1) - convertToDouble(num2);
+    }
+
+    @RequestMapping(value = "/multiply/{num1}/{num2}", method = RequestMethod.GET)
+    public Double multiply(
+        @PathVariable(value = "num1") String num1,
+        @PathVariable(value = "num2") String num2) throws Exception{
+
+        if(!isNumeric(num1) || !isNumeric(num2)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(num1) * convertToDouble(num2);
+    }
+
+    @RequestMapping(value = "/divide/{num1}/{num2}", method = RequestMethod.GET)
+    public Double divide(
+        @PathVariable(value = "num1") String num1,
+        @PathVariable(value = "num2") String num2) throws Exception{
+
+        if(!isNumeric(num1) || !isNumeric(num2)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(num1) / convertToDouble(num2);
+    }
+
+    @RequestMapping(value = "/avg/{num1}/{num2}", method = RequestMethod.GET)
+    public Double avg(
+        @PathVariable(value = "num1") String num1,
+        @PathVariable(value = "num2") String num2) throws Exception{
+
+        if(!isNumeric(num1) || !isNumeric(num2)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return (convertToDouble(num1) + convertToDouble(num2)) / 2;
+    }
+
+    @RequestMapping(value = "/sqrt/{num1}", method = RequestMethod.GET)
+    public Double sqrt(
+        @PathVariable(value = "num1") String num1) throws Exception{
+
+        if(!isNumeric(num1)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return Math.sqrt(convertToDouble(num1));
     }
 
     private Double convertToDouble(String num) throws Exception {
