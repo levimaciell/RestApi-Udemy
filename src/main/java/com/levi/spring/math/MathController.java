87ac7579d6ddb3eb2e6c853d9e57b746b1cbaf2c
levimaciell <levimaciell1221@gmail.com>
2024-02-15 16:38:17 -0300

    Sum working

--- src/main/java/com/levi/spring/math/MathController.java
+++ src/main/java/com/levi/spring/math/MathController.java
@@ -10,10 +10,26 @@ public class MathController {
     
     @RequestMapping(value = "/sum/{num1}/{num2}", method = RequestMethod.GET)
     public Double sum(
-        @PathVariable(value = "num1") Double num1,
-        @PathVariable(value = "num2") Double num2){
+        @PathVariable(value = "num1") String num1,
+        @PathVariable(value = "num2") String num2) throws Exception{
 
-        return 1D;
+        if(!isNumeric(num1) || !isNumeric(num2)){
+            throw new Exception();
+        }
+        return convertToDouble(num1) + convertToDouble(num2);
+    }
+
+    private Double convertToDouble(String num) throws Exception {
+        if(num == null) throw new Exception();
+        String number = num.replaceAll(",", ".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+        else throw new Exception();
+    }
+
+    private boolean isNumeric(String num) throws Exception {
+        if(num == null) throw new Exception();
+        String number = num.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 
 }
