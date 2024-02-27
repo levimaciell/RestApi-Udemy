099c6ef41d9971f5e9c15c5a893adfc3de89f543
levimaciell <levimaciell1221@gmail.com>
2024-02-27 15:36:12 -0300

    Adding v2 version of create endpoint

---
+++ src/main/java/com/levi/spring/person/dto/PersonDto.java
@@ -0,0 +1,40 @@
+package com.levi.spring.person.dto;
+
+public class PersonDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+    
+    public PersonDto(Long id, String firstName, String lastName, String address, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    
+}
